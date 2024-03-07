c366e8b3a01a1079eb07e173bea40f6c0407daa2
Tash <shitashni@gmail.com>
2024-03-08 01:22:27 +1300

    first commit

---
+++ UnitTestProject1/LoginPage.cs
@@ -0,0 +1,43 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProject1
+{
+
+
+     class LoginPage
+    {
+        private IWebDriver driver;
+        public LoginPage(IWebDriver browser)
+        {
+            driver = browser;
+        }
+
+        private By login = By.CssSelector("a[class='nav-link text-dark1']");
+        private IWebElement BtnLogin => driver.FindElement(login);
+
+        private By username = By.Id("Input_Email");
+        private IWebElement Username => driver.FindElement(username);
+
+        private By password = By.Id("Input_Password");
+        private IWebElement Password => driver.FindElement(password);
+        private By loginClick = By.Id("login-submit");
+        private IWebElement BtnLoginClick => driver.FindElement(loginClick);
+        
+        public void GoToLogin()
+        {
+            BtnLogin.Click();
+        }
+
+        public void LoginApplication(string username, string password)
+        {
+            Username.SendKeys(username);
+            Password.SendKeys(password);
+            BtnLoginClick.Click();
+        }
+    }
+}
---
+++ UnitTestProject1/LoginTest.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class LoginTest
+    {
+        private IWebDriver driver;
+        private LoginPage login;
+        [TestInitialize] 
+        public void Setup() { 
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("https://localhost:7091/");
+            login = new LoginPage(driver);
+        }
+
+        [TestMethod]
+        public void Login()
+        {
+            login.GoToLogin();
+            login.LoginApplication("test2@test.com", "Admin@123");
+        }
+    }
+}
